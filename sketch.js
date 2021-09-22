bbcaff9e20a2f0827e9636757621b94360a5ff4f
RoutineClient77 <77229720+RoutineClient77@users.noreply.github.com>
2021-09-22 17:32:43 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -7,7 +7,9 @@ var quest, questImg;
 var captain, captainImg;
 var advice, adviceImg;
 var gremlin, gremlinImg, gremlinGroup;
-
+var flat = 0;
+var ghost, ghostImg;
+var c,cImg
 
 function preload(){
   
@@ -25,13 +27,15 @@ function preload(){
   captainImg = loadImage("Captain Gibraltar.PNG");
   adviceImg = loadImage("advice (2).png");
   gremlinImg = loadImage("Gremlin.PNG");
+  ghostImg = loadImage("Gremlin.PNG");
+  cImg = loadImage("challenge.PNG");
 }
 
 function setup() {
 
   
   createCanvas(1500,1040);
-
+   
   //adding the background image
   bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
 bg.addImage(bgImg)
@@ -39,21 +43,22 @@ bg.scale = 1.1
   
 
 //creating the player sprite
-player = createSprite(100, 900, 50, 50);
+player = createSprite(100, 1000, 50, 50);
  player.addImage(warlock)
    player.scale = 0.3
    
    player.setCollider("rectangle",0,0,300,300)
-  
-
+  player.debug = true;
+  spawnC();
+  spawnQuest();
 }
 
 function draw() {
   background(0); 
   
-   spawnQuest();
-  console.log(player.x);
-  console.log(player.y);
+  //  spawnQuest();
+  // console.log(player.x);
+  // console.log(player.y);
     
   //moving the player up and down and making the game mobile compatible using touches
 if(keyDown("UP_ARROW")||touches.length>0){
@@ -99,13 +104,43 @@ if(keyWentDown("C")){
  
   player.addImage(hunterS)
 }
+// if(player.y <= 500){
+//   shaw.visiblity = "hidden";
+//   quest.visible = false;
+//   advice.visible = false;
+//   captain.visible = false;
 
+// }
+console.log(player.x+","+player.y);
+if(player.x > 1000 && player.y > 1000){
+  shaw.visible = true;
+  quest.visible = true;
+  advice.visible = true;
+  captain.visible = true;
+} 
+if(player.x < 1000 && player.y < 1000){
+  shaw.visible = false;
+  quest.visible = false;
+  advice.visible = false;
+  captain.visible = false;
+
+   
+
+  console.log("hi");
+} 
 
 drawSprites();
 
 }
-function spawnQuest(){
-  if(player.x >= 700 && player.y >= 600){
+
+  
+  
+    
+   
+    
+  
+  function spawnQuest(){
+    console.log("spawnquest")
     shaw = createSprite(1400,810,50,50);
     shaw.addImage(shawImg)
     shaw.scale = 0.8;
@@ -118,19 +153,17 @@ function spawnQuest(){
     captain.addImage(captainImg);
     captain.scale = 0.5;
     
-  } 
-  
-  if(player.y <= 500){
-    
-    if(player.y <= 500){
-      shaw.visiblity = "hidden";
-      quest.visible = false;
-      advice.visible = false;
-      captain.visible = false;
-  
-    }
-  }
   
   
   
+}
+function spawnC(){
+ if( player.y = 490){
+  c = createSprite(910,350,50,50);
+  c.addImage(cImg)
+  c.scale = 1;
+  ghost = createSprite(910,490,50,50);
+  ghost.addImage(ghostImg)
+  ghost.scale = 0.5;
+ }
 }
